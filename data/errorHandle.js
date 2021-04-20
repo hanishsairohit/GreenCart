5172868d843a49495ee457b55946d71cb22b013d
mdevika-70 <dmhatre@stevens.edu>
2021-04-20 00:14:30 -0400

    test

--- data/errorHandle.js
+++ data/errorHandle.js
@@ -1,13 +1,14 @@
 //Function for checking the string.
 
 function stringCheck(str) {
-  if (!str || typeof str !== "string" || str.trim().length == 0) return true;
-  else return false;
+  (!str || typeof str !== "string" || str.trim().length == 0) ?false:true;
+
 }
 
-//email validation function
+//email validation functions
 // return boolean
 function emailValidate(email) {
+  
   if (!email) return false;
   const emailRe = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return emailRe.test(toString(email).toLowerCase());
