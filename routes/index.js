6659a59b38ef5569575891bddc0c3d6e6cdd1d4d
dhruveeldoshi <dhruveeldoshi@gmail.com>
2021-04-27 13:06:34 -0400

    new add

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 // const mainRoutes = require('./routes');
-const productsRoutes = require('./products');
-const path = require('path');
+const productsRoutes = require("./products");
+const path = require("path");
 
 const constructorMethod = (app) => {
-//   app.use('/', mainRoutes);
-  app.use('/products', productsRoutes);
+  //   app.use('/', mainRoutes);
+  app.use("/products", productsRoutes);
+  app.use("/", (req, res) => {
+    res.render("pages/test");
+  });
 };
 
 module.exports = constructorMethod;
