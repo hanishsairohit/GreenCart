19ad4cc72412719da782df13b00fb88297a0bf8c
dhruveeldoshi <dhruveeldoshi@gmail.com>
2021-04-28 19:23:20 -0400

    user signup route(Partial)

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 // const mainRoutes = require('./routes');
-const productsRoutes = require('./products');
-const path = require('path');
+const productsRoutes = require("./products");
+const path = require("path");
 
 const constructorMethod = (app) => {
-//   app.use('/', mainRoutes);
-  app.use('/products', productsRoutes);
+  //   app.use('/', mainRoutes);
+  app.use("/products", productsRoutes);
+  app.use(
+    "*",
+    res.status(404).render("404: No Page Found!!", { title: "404: Error" })
+  );
 };
 
 module.exports = constructorMethod;
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const data = require("../data");
 usersData = data.users;
+const xss = require("xss");
+errorCheck = require("../errorCheck");
 
 router.get("/", async (req, res) => {
   try {
@@ -21,11 +23,51 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/:id", async (req, res) => {
-  try {
-  } catch (error) {
-    res.status().json({ message: error });
+router.post("/signup", async (req, res) => {
+  const firstName = xss(req.body.firstName);
+  const lastName = xss(req.body.lastName);
+  const email = xss(req.body.email);
+  const password = xss(req.body.password);
+  const phoneNumber = xss(req.body.phoneNumber);
+  const address = xss(req.body.address);
+
+  errors = [];
+  if (!errorCheck.stringCheck(firstName))
+    errors.push("Invalid FirstName (routes/users)");
+  if (!errorCheck.stringCheck(lastName))
+    errors.push("Invalid LastName (routes/users)");
+  if (!errorCheck.emailValidate(email))
+    errors.push("Invalid Email (routes/users)");
+  if (!errorCheck.validPassword(password))
+    errors.push("Invalid Password (routes/users)");
+  if (!errorCheck.phoneNumberValid(phoneNumber))
+    errors.push("Invalid PhoneNumber (routes/users)");
+
+  // Just for woking part I'm throwing JSON error
+  if (errors.length > 0) {
+    return res.status(401).json({ errors: "Erros while fil" });
   }
+  try {
+    const newUser = await userData.addUser(
+      firstName,
+      lastName,
+      phoneNumber,
+      emailId,
+      password,
+      address
+    );
+    req.seesion.user = newUser;
+    //Just for now redirecting to the root route
+    res.redirect("/");
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
+
+router.get("/logout", async (req, res) => {
+  req.session.destroy();
+  // req.session.user=null;
+  res.redirect("/");
 });
 
 module.exports = router;
