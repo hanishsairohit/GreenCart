3b9b1cc0cf2b8b7b3e0a66161a72f2c398389c2f
Hanish Sai Rohit <hanishsairohit@hanishs-air.myfiosgateway.com>
2021-05-09 20:45:01 -0400

    implemented few routes

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const constructorMethod = (app) => {
   // app.use("/", (req, res) => {
   //   return res.render("pages/home", { title: "In Home" });
   // });
-  app.use("/products", productsRoutes);
+  app.use("/", productsRoutes);
   app.use("/users", userRoutes);
   app.use("*", (req, res) => {
     res.status(404).json({ title: "404: Error" });
